9a9d1f620dee9fcdbf3bbcc3e49e93345c2338af
Jabari Brown <brownj47@tcnj.edu>
2019-01-08 22:07:45 -0500

    Remaking an old app idea

--- Learning/App.js
+++ Learning/App.js
@@ -6,26 +6,46 @@
  * @flow
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TextInput} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, TextInput, Picker } from 'react-native';
 
-export default class PizzaTranslator extends Component {
+export default class OneTwoHelp extends Component {
   constructor(props) {
     super(props);
-    this.state = {text: ''};
+    this.state = { text: '' };
   }
 
   render() {
     return (
-      <View style={{padding: 10}}>
+      <View style={{ padding: 10 }}>
+        
+        <Picker
+          selectedValue={this.state.language}
+          style={{ height: 50, width: 100 }}
+          onValueChange={(itemValue, itemIndex) => this.setState({ language: itemValue })}>
+          <Picker.Item label="Male" value="m" />
+          <Picker.Item label="Female" value="f" />
+          <Picker.Item label="Unsure" value="u" />
+        </Picker>
+
+        <Picker
+          selectedValue={this.state.language}
+          style={{ height: 50, width: 100 }}
+          onValueChange={(itemValue, itemIndex) => this.setState({ language: itemValue })}>
+          <Picker.Item label="Young" value="y" />
+          <Picker.Item label="Adult" value="a" />
+          <Picker.Item label="Elder" value="o" />
+        </Picker>
+
         <TextInput
-          style={{height: 40}}
-          placeholder="Type here to translate!"
-          onChangeText={(text) => this.setState({text})}
+          style={{ height: 40 }}
+          placeholder="Location"
         />
-        <Text style={{padding: 10, fontSize: 42}}>
-          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-        </Text>
+
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="Additional information"
+        />
       </View>
     );
   }
